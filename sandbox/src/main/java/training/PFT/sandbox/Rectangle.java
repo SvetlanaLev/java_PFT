1fe3ee1c783ec34295f2fdbf1d7f5cc9e7820145
SvetlanaLev <tyev_s@mail.ru>
2016-06-16 20:44:03 +0300

    Classes usage

---
+++ sandbox/src/main/java/training/PFT/sandbox/Rectangle.java
@@ -0,0 +1,9 @@
+package training.PFT.sandbox;
+
+/**
+ * Created by User on 6/16/2016.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+}
---
+++ sandbox/src/main/java/training/PFT/sandbox/Square.java
@@ -0,0 +1,8 @@
+package training.PFT.sandbox;
+
+/**
+ * Created by User on 6/16/2016.
+ */
+public class Square {
+  public double l;
+}
