f981952e8f3ec3336de3c3e58c7df06bc0ee2c77
SvetlanaLev <tyev_s@mail.ru>
2016-06-27 22:57:07 +0300

    AddNewContacts after Refactoring

---
+++ addressbook_web_tests/src/test/java/training/PFT/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package training.PFT.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String nickname;
+  private final String company;
+  private final String address;
+  private final String notes;
+
+  public ContactData(String firstname, String middlename, String lastname, String nickname, String company, String address, String notes) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.company = company;
+    this.address = address;
+    this.notes = notes;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+}
